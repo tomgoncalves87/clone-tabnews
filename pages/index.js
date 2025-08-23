4859125a23e3ed657f55eec40a19bdef36842697
tomgoncalves87 <tomgoncalves87@gmail.com>
2025-08-23 15:01:25 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
-function Home(){
-    return <h1>Amor. Obrigado por ser a melhor coisa da minha vida e me dar a maior felicidade que vou ter! Te amo pra sempre!!!</h1>
+function Home() {
+  return (
+    <h1>
+      Amor. Obrigado por ser a melhor coisa da minha vida e me dar a maior
+      felicidade que vou ter! Te amo pra sempre!!!
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
